ceb408efd3b613de4c78a28a9e3530256c65fd95
ziyuan <hanziyuan08@outlook.com>
2021-07-26 20:42:52 +0800

    local IP returned now

--- README.md
+++ README.md
@@ -7,6 +7,10 @@ curl http://localhost:8080/ping
 The response you will get:
 ```json
 {
+  "addr": [
+    "192.168.31.10",
+    "192.168.194.10"
+  ],
   "message": "pong"
 }
 ```
--- main.go
+++ main.go
@@ -1,13 +1,36 @@
 package main
 
-import "github.com/gin-gonic/gin"
+import (
+	"fmt"
+	"github.com/gin-gonic/gin"
+	"net"
+)
 
 func main() {
 	r := gin.Default()
 	r.GET("/ping", func(c *gin.Context) {
 		c.JSON(200, gin.H{
 			"message": "pong",
+			"addr":    getIp(),
 		})
 	})
 	r.Run()
+}
+
+func getIp() []string {
+	addrs, err := net.InterfaceAddrs()
+	if err != nil {
+		fmt.Println(err)
+		return nil
+	}
+	var ret []string
+	for _, address := range addrs {
+		// 检查ip地址判断是否回环地址
+		if ipnet, ok := address.(*net.IPNet); ok && !ipnet.IP.IsLoopback() {
+			if ipnet.IP.To4() != nil {
+				ret = append(ret, ipnet.IP.String())
+			}
+		}
+	}
+	return ret
 }
